ad1b578026917205d5bc1a0848a3c4191c865c18
anatolyefimov <efimo.tolik@gmail.com>
2020-04-03 19:55:58 +0300

    Balanced binary treee

--- Task 2/BinaryTree.js
+++ Task 2/BinaryTree.js
@@ -1,6 +1,7 @@
 class Node {
     constructor(value) {
         this.value = value;
+        this.height = 1;
         this.left = null;
         this.right = null;
     }
@@ -15,7 +16,7 @@ class BinaryTree {
         }
     }
 
-    size() {
+    getSize() {
         return this._size;
     }
 
@@ -23,6 +24,12 @@ class BinaryTree {
         return this._size === 0;
     }
 
+    getHeight(node = this.root) {
+        if (!node) {
+            return 0;
+        }
+        return node.height;
+    }
     _getMinimum(node) {
         while (node.left) {
             node = node.left;
@@ -31,16 +38,18 @@ class BinaryTree {
         return node.value;
     }
 
+    getBalance(node) {
+        return node.left.height - node.right.height;
+    }
 
     insert(value) {
         ++this._size;
         this.root = this._insert(this.root, value);
     }
 
+
+
     delete(value) {
-        if (!this.isEmpty()) {
-            --this._size;
-        }
 
         this.root = this._delete(this.root, value);
     }
@@ -53,6 +62,27 @@ class BinaryTree {
             node.left = this._insert(node.left, value);
         } else {
             node.right = this._insert(node.right, value);
+        }
+
+        node.height = Math.max(this.getHeight(node.left), this.getHeight(node.right)) + 1;
+        const balance = node.left - node.right;
+
+        if (balance > 1 && value < node.left.key)  {
+            return this.rightRotate(node);
+        }
+
+        if (balance < -1 && value > node.right.key) {
+            return this.leftRotate(node);
+        }
+
+        if (balance > 1 && value > node.left.key)  {
+            node.left = this.leftRotate(node.left);
+            return this.rightRotate(node);
+        }
+
+        if (balance < -1 && value < node.right.key) {
+            node.right = this.rightRotate(node.right);
+            return this.leftRotate(node);
         }
         return node;
     }
@@ -77,9 +107,58 @@ class BinaryTree {
                 } else {
                     node = null;
                 }
+                --this._size;
             }
         }
+
+        if (!node)  {
+            return node;
+        }
+        node.height = Math.max(this.getHeight(node.left), this.getHeight(node.right)) + 1;
+        const balance = node.left - node.right;
+        if (balance > 1 &&  this.getBalance(node.left) >= 0)  {
+            return this.rightRotate(node);
+        }
+
+        if (balance > 1 &&  this.getBalance(node.left) < 0)    {
+            node.left = this.leftRotate(node.left);
+            return this.rightRotate(node);
+        }
+
+        if (balance < -1 && this.getBalance(node.right) <= 0) {
+            return this.leftRotate(node);
+        }
+
+        if (balance < -1 && this.getBalance(node.right) > 0)  {
+            node.right = this.rightRotate(node.right);
+            return this.leftRotate(node);
+        }
+
         return node;
+    }
+
+    rightRotate(y) {
+        const x = y.left;
+        const t2 = x.right;
+        x.right = y;
+        y.left = t2;
+
+        y.height = Math.max(this.getHeight(y.left), this.getHeight(y.right)) + 1;
+        x.height = Math.max(this.getHeight(x.left), this.getHeight(x.right)) + 1;
+
+        return x;
+    }
+
+    leftRotate(x)  {
+        const y = x.right;
+        const t2 = y.left;
+        y.left = x;
+        x.right = t2;
+
+        x.height = Math.max(this.getHeight(x.left), this.getHeight(x.right)) + 1;
+        y.height = Math.max(this.getHeight(y.left), this.getHeight(y.right)) + 1;
+
+        return y;
     }
 }
 
--- Task 2/task2.js
+++ Task 2/task2.js
@@ -3,12 +3,19 @@ const BinaryTree = require('./BinaryTree');
 const binaryTree = new BinaryTree(3, 2, 1, 3, 5, 3, 3);
 
 binaryTree.insert(9);
-
 printTree(binaryTree.root);
-
 console.log('-----------------');
 
+binaryTree.delete(3);
+binaryTree.delete(9);
 printTree(binaryTree.root);
+console.log('-----------------');
+
+while(!binaryTree.isEmpty()) {
+    binaryTree.delete(binaryTree.root.value);
+}
+printTree(binaryTree.root);
+console.log('size: ', binaryTree.getSize() );
 
 function printTree(node) {
     if (!node) {
